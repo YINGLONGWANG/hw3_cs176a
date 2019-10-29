5cf8b44764811df5f0b450c31ac73047a2121844
Maggie Lim <maggielim@cs.ucsb.edu>
2019-10-28 21:14:23 -0700

    able to send ping seq number and time to server

--- pingclient.c
+++ pingclient.c
@@ -6,7 +6,7 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <sys/time.h>
-char * get_message();
+char * get_message(int seq_num);
 
 
 int main(int argc, char * argv[]) {
@@ -15,12 +15,27 @@ int main(int argc, char * argv[]) {
 	char server_ip[] = "127.0.0.1";
 	int port = 12000;
 	
-	const char * message = get_message();
+	const char * message = get_message(1);
 	int socket_id = socket(AF_INET, SOCK_DGRAM, 0);
 
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = inet_addr( server_ip );
 	server_address.sin_port = htons(port);
+	
+/*	int i = 0;
+	while(i<10)
+	{
+		
+
+
+
+
+
+	}
+
+
+
+*/
 	printf("Sending '%s' to %s:%i\n", message, server_ip, port);
 	sendto( socket_id, message, strlen(message), 0,
 		(struct sockaddr *) &server_address, sizeof(struct sockaddr_in) );
@@ -32,17 +47,26 @@ int main(int argc, char * argv[]) {
 	close(socket_id);
 }
 
-char * get_message(){
+char * get_time(){
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	long double millisecondsSinceEpoch =
-    		(long double)(tv.tv_sec) * 1000 +
-    		(long double)(tv.tv_usec) / 1000;
-	char* time = malloc(100);
-	sprintf(time, "%Lf", millisecondsSinceEpoch);
+ 	long double millisecondsSinceEpoch =
+		(long double)(tv.tv_sec) * 1000 +
+ 		(long double)(tv.tv_usec) / 1000;
+ 	char* time = malloc(100);
+        sprintf(time, "%Lf", millisecondsSinceEpoch);
 
+	return time;
+}
+
+char * get_message(int seq_num){
+	char * time = get_time();
 	char * message= malloc(400);
+	char * number = malloc(4);
+	sprintf(number, "%d", seq_num);
 	strcpy(message, "PING ");
+	strcat(message, number);
+	strcat(message, " ");
 	strcat(message, time);
 	
 
